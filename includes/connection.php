4bbfc428a386520398293fb33cd3c320278d6a8d
deependerrathore <deepak.republic@gmail.com>
2014-02-13 15:06:14 +0530

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,5 +1,5 @@
 <?php
-	$connection = mysql_connect("mysql.1freehosting.com","u430836713_chp","deepak28") or die(mysql_error());
+	$connection = mysql_connect("localhost","admin","") or die(mysql_error());
 	
 	$select_db = mysql_select_db("u430836713_chp") or die(mysql_error());
 
